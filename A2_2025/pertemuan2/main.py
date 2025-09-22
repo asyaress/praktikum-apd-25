c49b19963142940d069b67252af15e426fa8d527
Athasyahri Syawal Fahrezy <athafarez2412@gmail.com>
2025-09-23 01:56:59 +0800

    tambah pertemuan 2

---
+++ A2_2025/pertemuan2/main.py
@@ -0,0 +1,78 @@
+# nama = "Daffa"
+# Nama = "Nabil"
+
+# print(nama, Nama)
+
+# print ("""Halo,
+# Saya sedang belajar ngoding
+# Bahasa Pemrograman python""")
+
+# print("halo ""saya"" daffa")
+# print("halo "+"daffa")
+
+# teks = 'saya suka belajar apd'
+# print(teks[3])
+# print(teks[0:3])
+# print(teks[5:])
+# print(teks[:8])
+
+# Hujan = False
+# Panas = True
+
+# hasil = Hujan and Panas
+# print(hasil)
+
+# prodi = ['Informatika', 'Sistem Informasi', 3 , 0.2999 ,'Teknik Sipil']
+# print (prodi[0])
+# print(prodi[1])
+# print(prodi[2])
+# print(prodi[3])
+# print(prodi)
+
+# tuple = ('Lian', 19, True, 'APD')
+# print(tuple[0])
+# print(tuple[2])
+
+# Biodata = {'nama1' : 'Daffa',
+#            'kelas' : 2025,
+#            'nama2'  : 'Arul',
+#            'kelas2' : 2024,
+#            'nama3' : 'Bambang',
+#            'kelas3' : 2023,
+#            }
+
+# print(Biodata['nama1'])
+# print(Biodata['kelas'])
+# print(Biodata['nama2'])
+# print(Biodata['kelas2'])
+# print(Biodata['nama3'])
+# print(Biodata['kelas3'])
+
+# angka = "123"
+# angka1 = int(angka)
+# print(type(angka1))
+
+# nama = input('masukkan Umur Anda : ')
+# print(nama)
+
+# panjanginput = input('masukkan panjang persegi : ')
+# lebarinput = input("maskkan lebar persegi : ")
+
+# panjang = int(panjanginput)
+# lebar = int(lebarinput)
+
+# luas = panjang * lebar
+
+# print(luas)
+
+# nilaisiswa = [85.5, 91.0, 77.5, 88.0]
+# print (nilaisiswa[0])
+# print(nilaisiswa[1])
+# print(nilaisiswa[2])
+# print(nilaisiswa[3])
+
+# print (nilaisiswa[0] + nilaisiswa[1] + nilaisiswa[2] + nilaisiswa[3])
+
+# harga = int(input('Berapa total belanja mu : '))
+# hargapajak = (harga * 0.11)
+# total = (harga + hargapajak)
